13baa25e979baf36da902a20d3d2e890c83973fe
valerio <valerio.deprosperi@gmail.com>
2020-08-27 19:20:31 +0200

    var prompt e check

--- js/script.js
+++ js/script.js
@@ -1,2 +1,9 @@
 // Chiedere all’utente di inserire una parola
 // Creare una funzione per capire se la parola inserita è palindroma
+
+// 1.chiedo a utente di inserire la parola
+var parola = prompt("inserisci una parola");
+// 2. creo var per controllo parola
+var check = checkPalindrom(parola);
+
+// 3. scrivo se la parola è o non è un palindromo
